198f73b93b7856762c808f48fef9d51ffc90db13
bclement-ocp <129742207+bclement-ocp@users.noreply.github.com>
2024-07-23 08:39:34 +0000

    Deploying to gh-pages from  @ bc6516e73363c87ef6b53f43b8869002f783c64e 🚀

---
+++ multidoc/_static/js/versions.js
@@ -0,0 +1,29 @@
+function renderVersions(versions) {
+  return `
+  <dl>
+    <dt>Versions</dt>${ versions.active.map((version) => `
+    <dd ${ version.slug == versions.current.slug ? 'class="rtd-current-item"' : '' }>
+      <a href="${ version.url }">${ version.slug }</a>
+    </dd>
+    `).join("\n")}
+  </dl>
+  `;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  $.getJSON('_static/versions.json', (versions) => {
+    console.log(versions);
+    document.body.insertAdjacentHTML("beforeend", `
+    <div class="rst-versions" data-toggle="rst-versions" role="note" aria-label="Versions">
+      <span class="rst-current-version" data-toggle="rst-current-version">
+        <span class="fa fa-book"> Current version</span>
+        v: ${ versions.current.slug }
+        <span class="fa fa-caret-down"></span>
+      </span>
+      <div class="rst-other-versions">
+        ${renderVersions(versions)}
+      </div>
+    </div>
+    `);
+  });
+});
